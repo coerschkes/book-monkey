304b2f2fbfa1d056758df42fd6b707baa0e47cd7
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-08-13 09:40:07 +0200

    feat: Implement the book-list component.

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Book} from "../shared/book";
 
 @Component({
   selector: 'bm-book-list',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
+  books: Book[] = [];
 
+  constructor() {
+    this.books = [
+      {
+        isbn: '12345',
+        title: 'Tierisch gut kochen',
+        authors: ['Mrs Chimp', 'Mr Gorilla'],
+        published: '2022-06-20',
+        subtitle: 'Rezepte von Affe bis Zebra',
+        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
+        description: 'Immer lecker und gut'
+      },
+      {
+        isbn: '67890',
+        title: 'Backen mit Affen',
+        authors: ['Orang Utan'],
+        published: '2022-07-15',
+        subtitle: 'Bananenbrot und mehr',
+        thumbnailUrl: 'https://cdn.ng-buch.de/backen.png',
+        description: 'Tolle Backtipps für Mensch und Tier'
+      }
+    ];
+  }
 }
